46fbc242b230f699ce6c4330308b8d718e8dbb42
Yousef Borhani <yousefborhanii@gmail.com>
2020-05-17 21:53:30 +0430

    add a UI design for ending , add a box collider for finish spot and write a script for ending the game

---
+++ Assets/Scripts/GameEnding.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+    
+    bool m_IsPlayerAtExit;
+    float m_Timer;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            m_IsPlayerAtExit = true;
+        }
+    }
+
+    private void Update()
+    {
+        if(m_IsPlayerAtExit)
+        {
+            EndLevel ();
+        }
+    }
+    void EndLevel ()
+    {
+        m_Timer += Time.deltaTime;
+        exitBackgroundImageCanvasGroup.al
+        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+        if(m_Timer > fadeDuration + displayImageDuration)
+        {
+            Application.Quit();
+        }
+    }
+}
